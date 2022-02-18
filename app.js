3f0643817a95f10857b255523269fe61f2c22367
Mohamad Krayem <mk2242682@gmail.com>
2022-02-18 19:13:54 +0200

    add some middleware into utils/middleware.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const logger = require('./utils/logger');
 const { mongoUrl, PORT } = require('./utils/config');
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware');
 
 mongoose.connect(mongoUrl)
   .then(() => {
@@ -16,7 +17,9 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
+app.use(middleware.requestLogger);
 app.use('/',blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
